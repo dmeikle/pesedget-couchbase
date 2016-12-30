d479e63af32bcdc28b10934a09842a572dcfbb52
Dave Meikle <david@quantumunit.com>
2016-12-30 14:34:47 -0800

    added setInactive command

---
+++ src/Gossamer/Pesedget/Extensions/Couchbase/Client/Commands/SetInactiveCommand.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ *  This file is part of the Quantum Unit Solutions development package.
+ * 
+ *  (c) Quantum Unit Solutions <http://github.com/dmeikle/>
+ * 
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+/** *
+ * Author: dave
+ * Date: 12/30/2016
+ * Time: 2:27 PM
+ */
+
+namespace Gossamer\Pesedget\Extensions\Couchbase\Client\Commands;
+
+
+class SetInactiveCommand  extends AbstractCouchbaseCommand
+{
+
+    public function execute($params = array(), $request = array())
+    {
+        $queryString = "UPDATE " . $this->getBucketName() . " SET isActive = '0' use keys ('" . $request['id'] . "')";
+
+        $query = \CouchbaseN1qlQuery::fromString($queryString);
+
+        $rows = $this->getBucket()->query($query);
+
+        $this->httpRequest->setAttribute('result', array('success'=>'true', 'code'=> '200'));
+    }
+}
